5152e7dc2b1f5b3cd10a8d500b97ed128efdb8ec
Ramon Rosa da Silva <ramon.silva>
2018-08-12 19:22:20 -0300

    .

--- src/main/java/mock/WebConfig.java
+++ src/main/java/mock/WebConfig.java
@@ -2,7 +2,6 @@ package mock;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -12,7 +11,6 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
-@EnableWebMvc
 public class WebConfig {
     
     @Bean
